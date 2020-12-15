e67af0e4463bf6954ad9c8b166cd26aa8e817e51
ALEX ENRIQUEZ <alexis.enriquezca@gmail.com>
2020-12-15 17:01:44 -0500

    feat: Adding Seed data

---
+++ Server/Controllers/SpecialsController.cs
@@ -0,0 +1,30 @@
+﻿using BlazingPizza.Server.Models;
+using BlazingPizza.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SpecialsController : ControllerBase
+    {
+        private readonly PizzaStoreConext _context;
+
+        public SpecialsController(PizzaStoreConext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PizzaSpecial>>> GetSpecials()
+        {
+            return await _context.Specials
+                .OrderByDescending(s => s.BasePrice).ToListAsync();
+        }
+    }
+}
---
+++ Server/Models/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using BlazingPizza.Shared;
+
+namespace BlazingPizza.Server.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(PizzaStoreConext context)
+        {
+            var specials = new PizzaSpecial[]
+            {
+                new PizzaSpecial
+                {
+                    Name= "Pizza clásica de queso",
+                    Description = "Es de queso y delicioso. ¿Por qué no querrías una?",
+                    BasePrice = 189.9m,
+                    ImageUrl="images/pizzas/cheese.jpg"
+                },
+                new PizzaSpecial
+                {
+                    Name= "Tocinator",
+                    Description = "Tiene todo tipo de tocino",
+                    BasePrice = 227.99m,
+                    ImageUrl="images/pizzas/bacon.jpg"
+                },
+                new PizzaSpecial
+                {
+                    Name= "Clásica de perperoni",
+                    Description = "Peperoniana",
+                    BasePrice = 189.9m,
+                    ImageUrl="images/pizzas/cheese.jpg"
+                }
+            };
+
+            context.Specials.AddRange(specials);
+            context.SaveChanges();
+        }
+    }
+}
