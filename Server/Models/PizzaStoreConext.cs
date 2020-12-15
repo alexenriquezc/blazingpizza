877786e3727792e501aa4888f6319d28c51d854b
ALEX ENRIQUEZ <alexis.enriquezca@gmail.com>
2020-12-15 08:57:29 -0500

    chore: structuring the project architecture

---
+++ Server/Models/PizzaStoreConext.cs
@@ -0,0 +1,14 @@
+﻿using BlazingPizza.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Server.Models
+{
+    public class PizzaStoreConext: DbContext
+    {
+        public PizzaStoreConext(DbContextOptions<PizzaStoreConext> options): base(options)
+        {
+        }
+
+        public DbSet<PizzaSpecial> Specials { get; set; }
+    }
+}
