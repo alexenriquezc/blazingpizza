c96f9689ed8bff02ca44ade1215d54fcb6bf6a39
alex <alexis.enriquezca@gmail.com>
2020-12-16 00:05:40 -0500

    chore: Upgrade project from netcore 3.1 to net5

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,8 +11,9 @@ namespace BlazingPizza.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
+            //builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
+            
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
